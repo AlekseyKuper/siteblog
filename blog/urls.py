f04a263c89e4678cfef04a006cf1f0e427a19957
Aleksey <aleksk.ru95@mail.ru>
2023-11-08 14:47:23 +0300

    Завершение проекта

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('category/<str:slug>/', PostByCategory.as_view(), name='category'),
     path('post/<str:slug>/', Get_post.as_view(), name='post'),
+    path('tag/<str:slug>/', PostByTag.as_view(), name='tag'),
+    path('search/', Search.as_view(), name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post, Category, Tag
+from django.db.models import F
 
 class Home(ListView):
     model = Post
@@ -37,4 +38,33 @@ class Get_post(DetailView):
         context = super().get_context_data(**kwargs)
         self.object.views = F('views') + 1
         self.object.save()
+        self.object.refresh_from_db()
         return context
+
+class PostByTag(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tag__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу: ' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
+
+class Search(ListView):
+    template_name = 'blog/search.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+
+    def get_queryset(self):
+        return Post.objects.filter(title__icontains=self.request.GET.get('s'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['s'] = f"s={self.request.GET.get('s')}&"
+        return context
+
